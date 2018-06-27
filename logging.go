af3032c4f4df4585b2cc0bd8c5aa630bd8092cd3
songrgg <songrgg0.0@gmail.com>
2018-06-27 13:12:02 +0800

    add logging switch

--- logging.go
+++ logging.go
@@ -5,9 +5,19 @@ import (
 	"os"
 )
 
-const (
+var (
 	logCallbacks = false
 	utpLogging   = false
 )
 
 var Logger = log.New(os.Stderr, "go-libutp: ", log.LstdFlags|log.Lshortfile)
+
+// SetUTPLogging sets the utp logging open
+func SetUTPLogging(open bool) {
+	utpLogging = open
+}
+
+// SetUTPLogging sets the callback logging open
+func SetLogCallBacks(open bool) {
+	logCallbacks = open
+}
